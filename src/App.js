86e82fd69c22e53a1886405add76f8c06edeae40
Rafael <rafollo@live.com>
2022-03-19 19:50:16 -0300

    Adicionado mecanismo de transformação visual dos arrays

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import NavbarComponent from './components/navbar';
 import "bootstrap/dist/css/bootstrap.min.css";
+import BlockchainPage from './components/BlockchainPage/BlockchainPage';
 
 function App() {
   return (
     <div className="App">
       <NavbarComponent />
+      <BlockchainPage />
     </div>
-  );
+  )
 }
 
 export default App;
