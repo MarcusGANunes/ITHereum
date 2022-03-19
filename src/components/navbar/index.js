95065751124ae27b8c13531d560d234af55c2027
Marcus Nunes <marcusnunes_11@hotmail.com>
2022-03-19 16:45:56 -0300

    some changes

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import styles from './navbar.modules.css';
+import styles from './navbar.css';
 
 export default function NavbarComponent() {
   return (
