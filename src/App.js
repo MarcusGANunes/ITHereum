ce90edd605b2872eda75a4d439ea3e305e8444a3
Marcus Nunes <marcusnunes_11@hotmail.com>
2022-03-19 17:52:05 -0300

    bug in css solved

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import NavbarComponent from './components/navbar';
-import "bootstrap/dist/css/bootstrap.min.css";
+import FooterComponent from './components/footer';
+
 
 function App() {
   return (
     <div className="App">
       <NavbarComponent />
+      <FooterComponent />
     </div>
   );
 }
---
+++ src/components/footer/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./footer.modules.css";
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
+
+export default function FooterComponent() {
+  return (
+    <footer>
+      <div className="footer">
+        <ul className="social_list">
+          <li>
+            <FaFacebook />
+          </li>
+          <li>
+            <FaInstagram />
+          </li>
+          <li>
+            <FaLinkedin />
+          </li>
+        </ul>
+        <p className="copy_right">
+          ITHereum<span>&copy; 2022</span>
+        </p>
+      </div>
+    </footer>
+  );
+}
