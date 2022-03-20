a3619448a7f353e5aa023fe804c8930420e3e3ee
Rafael <rafollo@live.com>
2022-03-20 08:13:30 -0300

    Correção de bugs

--- src/components/card/card1.js
+++ src/components/card/card1.js
@@ -3,34 +3,29 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { ThemeProvider } from "@material-ui/core/styles";
-import customtheme from "./theme";
 
 export default function ImgMediaCard() {
-    return ( <
-        Card sx = {
-            { maxWidth: 370, height: 400 } } >
-        <
-        CardMedia component = "img"
-        alt = "Walter Melhado Arbiol Forné"
-        height = "140"
-        image = "walter.png" /
-        >
-        <
-        CardContent >
-        <
-        Typography gutterBottom variant = "h6"
-        component = "div" >
-        <
-        strong > Walter Melhado Arbiol Forné < /strong> <
-        /Typography> <
-        Typography variant = "body2"
-        color = "text.secondary" >
-        Beyounds the college, works mainly in web scraping services and in its free time studies about development and web 3.0 technologies.Got some prizes in scientific olympics on high school, as honorable mension in
-        brazilian computer olympics or a silver medal into Canguru 's
-        Mathmatics Olimpics, and as the others 2 pass into the hardest entrance exam of the Latin America(the ITA exam). <
-        /Typography> <
-        /CardContent> <
-        /Card>
-    );
+  return (
+    <Card sx={{ maxWidth: 370, height: 400 }}>
+      <CardMedia
+        component="img"
+        alt="Walter Melhado Arbiol Forné"
+        height="140"
+        image="walter.png"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h6" component="div">
+          <strong>Walter Melhado Arbiol Forné</strong>
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Beyounds the college, works mainly in web scraping services and in its
+          free time studies about development and web 3.0 technologies. Got some
+          prizes in scientific olympics on high school, as honorable mension in
+          brazilian computer olympics or a silver medal into Canguru's
+          Mathmatics Olimpics, and as the others 2 pass into the hardest
+          entrance exam of the Latin America (the ITA exam).
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 }
